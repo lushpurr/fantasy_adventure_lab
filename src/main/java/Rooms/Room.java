c1649ead35c82c19be82c616b663ef87f2244968
lushpurr <lushpurr@gmail.com>
2021-01-15 14:25:53 +0000

    updated room to take treasure and creature

--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,4 +1,32 @@
 package Rooms;
 
+import Creatures.Creature;
+import Treasure.Treasure;
+
 public class Room {
+    private String name;
+    private Treasure treasure;
+    private Creature creature;
+
+    public Room(String name, Treasure treasure, Creature creature){
+        this.name = name;
+        this.treasure = treasure;
+        this.creature = creature;
+    }
+
+    public Treasure getTreasure() {
+        return treasure;
+    }
+
+    public void setTreasure(Treasure treasure) {
+        this.treasure = treasure;
+    }
+
+    public Creature getCreature() {
+        return creature;
+    }
+
+    public void setCreature(Creature creature) {
+        this.creature = creature;
+    }
 }
